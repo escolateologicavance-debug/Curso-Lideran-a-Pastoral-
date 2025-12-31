1bd8dae459a81f4c71c7e950279ba70be041bed3
Gilberto Moreira <escolateologicavance@gmail.com>
2025-12-30 21:52:55 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'sincro-lideranca-v1';
+const assets = [
+  'index.html',
+  'avaliacao.html',
+  'personalizar.html',
+  'certificado_final.html',
+  'logo1-512.png',
+  'manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
